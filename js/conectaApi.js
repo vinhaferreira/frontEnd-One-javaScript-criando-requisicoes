ed7271810958a8f072e51fe2bc29d52f63403403
Flávia Ferreira <vinhaferreira@gmail.com>
2023-07-23 19:05:46 -0300

    Exibindo elementos

--- js/conectaApi.js
+++ js/conectaApi.js
@@ -1,7 +1,11 @@
 async function listaVideos() {
-  const conexao = await fetch("http://localhost:3000");
+  const conexao = await fetch("http://localhost:3000/videos");
   const conexaoConvertida = await conexao.json();
-  console.log(conexaoConvertida);
+
+  return conexaoConvertida;
 }
-listaVideos();
-//requisicao GET so quer receber os valores, por enquanto quer fazer nada
+export const conectaApi = {
+  listaVideos,
+};
+
+// //requisicao GET so quer receber os valores, por enquanto quer fazer nada
